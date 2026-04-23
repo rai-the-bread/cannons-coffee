15e2062fb6a322dc46565fe25c8fad9692f3b178
Rai Dorzback <raidorzback@gmail.com>
2026-04-23 12:47:27 -0400

    inquiry page data shows in console

--- js/custom-inquiry.js
+++ js/custom-inquiry.js
@@ -1,1 +1,12 @@
-console.log("Inquiry JS Working");
+const form = document.querySelector("#inquiry-form");
+
+function onFormSubmit(event) {
+  console.log("function is running");
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+  console.log(dataObject);
+  form.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
